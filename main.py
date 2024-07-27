d263c81a9d8db259c3bf36ff55fd48fd12173ef6
MedSaidBenAli <mohamed.sba@axelites.com>
2024-07-27 11:48:48 +0100

    conflit question 16

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+
+def somme(L):
+ s=0
+ for e in L:
+     s +=e
+ return
 my_list=[1,5,7]
+
 if my_list:
 	print('la somme est',sum(my_list))
 	print('le max est:',max(my_list))
 	print('le min est:',min(my_list))
 else:
 	print('liste vide')
+
+print('la somme est',sum(my_list))
+
+
